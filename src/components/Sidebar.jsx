96bd58add901b46f385def20a44be2dae8e1b5f2
ozymandias54 <bassirou718@gmail.com>
2024-07-26 16:58:18 +0000

    playlist

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {assets} from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import { UserContext } from '../context/UserContext';
 
 const Sidebar = () => {
 
     const navigate = useNavigate();
+    const {logout,login, etat,username,playliste}=useContext(UserContext)
 
   return (
     <div className='w-[425px] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
@@ -30,17 +32,21 @@ const Sidebar = () => {
                     <img className='w-5' src={assets.plus_icon}/>
                 </div>
             </div>
-            <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
-                <h1>Create your first play list</h1>
-                <p className='font-light'>It's easy we will help you</p>
-                <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Create Playlist</button>
-            </div>
-
-            <div className='mt-4 p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
-                <h1>Let's findsome podcasts to follow</h1>
-                <p className='font-light'>we'll keep you update on new episode</p>
-                <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Browse podcast</button>
-            </div>
+            {etat ? (
+                playliste.map((item, index)=>(
+                    <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
+                        <h1>{item.name}</h1>
+                        <p className='font-light'>It's easy we will help you</p>
+                        <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Create Playlist</button>
+                    </div>
+                ))
+            ) : (
+                <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
+                    <h1>Create your first play list</h1>
+                    <p className='font-light'>It's easy we will help you</p>
+                    <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Create Playlist</button>
+                </div>
+            )}
         </div>
     </div>
   )
--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { createContext, useState } from "react";
 
 export const UserContext=createContext()
@@ -6,17 +7,25 @@ const UserContextProvider= (props) => {
 
     const [playliste,setPlayliste]=useState([])
 
-    const [username,setUsername]=useState("John doe")
+    const [username,setUsername]=useState("")
 
     const [etat,setEtat]=useState(false)
 
+    const url = 'http://localhost:4000';
 
     const logout=()=>{
         setEtat(false)
     }
 
-    const login=()=>{
-        setEtat(true)
+    const login=async ()=>{
+        try {
+            const response = await axios.get(`${url}/api/album/list`);
+            setPlayliste(response.data.albums);
+            setUsername("John doe")
+            setEtat(true)
+        } catch (error) {
+            
+        }
     }
 
 
